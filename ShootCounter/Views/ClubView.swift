cd0204d9003189769fcaac3e115fae67a73cfa58
KT <thun.kristian@gmail.com>
2024-06-19 18:10:43 +0200

    Adding teams to a club and dismissing sheet

--- ShootCounter/Views/ClubView.swift
+++ ShootCounter/Views/ClubView.swift
@@ -8,11 +8,80 @@
 import SwiftUI
 
 struct ClubView: View {
-    var club: Club
+    @State var club: Club
+    @State var showAddTeamSheet = false
     
     
     var body: some View {
-        Text(club.name)
+        VStack{
+            ClubLogoImageView(imageData: club.logoData, maxHeight: 100)
+                .padding()
+            Form{
+                Section("Teams"){
+                    ForEach(club.teams){ team in
+                        Text(team.name)
+                    }
+                    .onDelete(perform: { indexSet in
+                        deleteTeam(indexSet)
+                    })
+                }
+            }
+            Button("Add Team"){
+               showAddTeamSheet = true
+            }
+        }
+        .navigationTitle(club.name)
+        .sheet(isPresented: $showAddTeamSheet, content:{
+            AddTeamSheet(club: $club)
+                .presentationDetents([.medium])
+        })
+
+        
+    }
+    func deleteTeam(_ indexSet: IndexSet){
+        for index in indexSet{
+            club.teams.remove(at: index)
+        }
+    }
+//    func deleteClub(_ indexSet: IndexSet){
+//        for index in indexSet {
+//            let club = clubs[index]
+//            modelContext.delete(club)
+//        }
+//    }
+}
+
+struct AddTeamSheet: View {
+    @Binding var club: Club
+    @State var name = ""
+    @Environment(\.modelContext) var modelContext
+//    @Environment(\.presentationMode.wrappedValue.dismiss()) var dismiss
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack{
+            Text("New Team")
+                .font(.title)
+                .bold()
+                .frame(alignment: .center)
+            Spacer()
+            TextField("Name of team:", text: $name)
+            Spacer()
+            Button("Save Team"){
+                saveTeam()
+                    
+            }
+        }
+        .padding()
+    }
+    func saveTeam(){
+        let team = Team(name: name)
+        if !club.teams.contains(where: {$0.name.lowercased() == team.name.lowercased() }){
+            club.teams.append(team)
+//                    modelContext.insert(club)
+            print(club)
+            dismiss()
+        }
     }
 }
 
